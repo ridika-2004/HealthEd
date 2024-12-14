9c117c477d59e34602d48d58c9386de900ec355a
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2024-12-14 07:31:28 +0600

    Deleted stupid HealthEd directory

--- HealthEd/src/source/Utility/IFileReadWrite.java
+++
@@ -1,8 +0,0 @@
-package source.Utility;
-
-public interface IFileReadWrite {
-    String readFile(String filePath);
-    boolean searchFromFile(String filePath, String searchTerm);
-    void writeToFile(String filePath, String data);
-    void appendToFile(String filePath, String data);
-}
